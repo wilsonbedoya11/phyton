7e5288f1d327811ee31419546f540ccf3fd7755f
wilson andres bedoya oquendo <wilsonbedoya11@gmail.com>
2025-03-12 02:31:14 +0000

    añadir graficos

--- App.py
+++ App.py
@@ -1,5 +1,44 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
+#import cyborg as cy
+#import matplotlib as plt
 
-st.write("Hello Mundo")
+dataframe = pd.DataFrame(
+    np.random.randn(10,20),
+    columns=('col %d' % i for i in range(20)))
 
+st.dataframe(dataframe.style.highlight_max(axis=0) )
+
+df_colombia_inversion_original = pd.read_html('https://datosmacro.expansion.com/estado/gasto/colombia')
+df_datos = pd.DataFrame(df_colombia_inversion_original[0])
+
+st.write(df_datos)
+
+
+chart_data = pd.DataFrame(
+    np.random.randn(20,3),
+    columns=['a','b','c'])
+
+st.line_chart(chart_data)
+
+
+
+map_data=pd.DataFrame(
+    np.random.randn(10,2) / [50,50] + [37.76, -122.4],
+    columns=['lat','lon'])
+
+st.map(map_data)
+
+
+map_data=pd.DataFrame(
+    np.random.randn(10,2) / [50,50] + [2.2476, 0.99],
+    columns=['lat','lon'])
+
+st.map(map_data)
+
+
+
+
+x = st.slider('x')
+st.write(x, 'squared is', x * x)
